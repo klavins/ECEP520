c1d4183b8d756090ab1042ff65f3d3155c4d2fc8
Eric Klavins <klavins@uw.edu>
2019-02-21 16:42:02 -0800

    published latest version of docs

--- docs/search/all_3.js
+++ docs/search/all_3.js
@@ -2,9 +2,9 @@ var searchData=
 [
   ['earliest',['earliest',['../classelma_1_1Channel.html#af2c53a031f99d8f7d5386158f801751c',1,'elma::Channel']]],
   ['elapsed',['elapsed',['../classelma_1_1Manager.html#ae6498164a8e790e474510f02618981d0',1,'elma::Manager']]],
-  ['emit',['emit',['../classelma_1_1Manager.html#a304a57ed61b1db9a1596bfeb8e7b0108',1,'elma::Manager']]],
-  ['empty',['empty',['../classelma_1_1Channel.html#a4367ee6a207ae6a88aac08eee69009f4',1,'elma::Channel']]],
+  ['emit',['emit',['../classelma_1_1Manager.html#a7dd4bc0f5774f6ac6c2dcf6c802d2418',1,'elma::Manager']]],
+  ['empty',['empty',['../classelma_1_1Channel.html#a4367ee6a207ae6a88aac08eee69009f4',1,'elma::Channel::empty()'],['../classelma_1_1Event.html#a73ba4c7ae6bc121f8ea42a3fde09ae0d',1,'elma::Event::empty()']]],
   ['event',['Event',['../classelma_1_1Event.html',1,'elma']]],
-  ['event',['Event',['../classelma_1_1Event.html#ab1d2a9774e489391c3b5d656b9420595',1,'elma::Event']]],
+  ['event',['Event',['../classelma_1_1Event.html#a2b1b7657420bbbebb81f1c59cc8fda96',1,'elma::Event']]],
   ['exception',['Exception',['../classelma_1_1Exception.html',1,'elma']]]
 ];
